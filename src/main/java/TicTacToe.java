80a87d8647897bca236a2b1146e061250ee99e02
Unknown <miron.kuzniar@gmail.com>
2017-10-11 10:54:41 +0200

    Adding code for Test
    Working code for X outside the board.

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,2 +1,8 @@
 public class TicTacToe {
+
+    public void play(int x, int y) {
+        if (x < 1 || x > 3) {
+            throw new RuntimeException("X placement outside the board");
+        }
+    }
 }
