516fb78dfe5841449ad61ed4ba097c6c1629842c
Unknown <miron.kuzniar@gmail.com>
2017-10-11 11:20:16 +0200

    Adding code for Test
    Working code for occupied field.

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,10 +1,16 @@
 public class TicTacToe {
+    private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
 
     public void play(int x, int y) {
         if (x < 1 || x > 3) {
             throw new RuntimeException("X placement outside the board");
         } else if (y < 1 || y > 3) {
             throw new RuntimeException("Y placement outside the board");
+        }
+        if (board[x - 1][y - 1] != '\0') { //table fields are starting numeration from 0, so x,y coordinates must be decreased by 1
+            throw new RuntimeException("Field is occupied");
+        } else {
+            board[x - 1][y - 1] = 'X';
         }
     }
 }
