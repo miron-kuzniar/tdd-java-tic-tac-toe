bb169b63711a835f8387b7c3d273a9f7cdc9a1a2
Unknown <miron.kuzniar@gmail.com>
2017-10-11 10:36:56 +0200

    Adding X Outside Board Test
    When X outside Board then we should see Runtime Exception

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class TicTacToeTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
 }
