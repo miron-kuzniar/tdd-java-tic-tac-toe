39ac4547a541d1128b0ac0edb65857df4c222a69
Unknown <miron.kuzniar@gmail.com>
2017-10-12 13:26:23 +0200

    Adding code for win conditions

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -5,8 +5,15 @@ public class TicTacToe {
     public String play(int x, int y) {
         checkAxis(x);
         checkAxis(y);
-        setBox(x, y);
         lastPlayer = nextPlayer();
+        setBox(x, y, lastPlayer);
+        for (int index = 0; index < 3; index++) {
+            if (board[0][index] == lastPlayer &&
+                board[1][index] == lastPlayer &&
+                board[2][index] == lastPlayer) {
+                return "Wygrał " + lastPlayer;
+            }
+        }
         return "Brak zwycięzcy";
     }
 
@@ -16,11 +23,11 @@ public class TicTacToe {
         }
     }
 
-    private void setBox(int x, int y) {
+    private void setBox(int x, int y, char lastPlayer) {
         if (board[x - 1][y - 1] != '\0') { //table fields are starting numeration from 0, so x,y coordinates must be decreased by 1
             throw new RuntimeException("Field is occupied");
         } else {
-            board[x - 1][y - 1] = 'X';
+            board[x - 1][y - 1] = lastPlayer;
         }
     }
 
