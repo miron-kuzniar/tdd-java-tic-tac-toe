c3cb201f94e50d02f0bfed5cd2f9583c0cdadbb7
Unknown <miron.kuzniar@gmail.com>
2017-10-12 12:38:29 +0200

    Adding code for move

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,10 +1,12 @@
 public class TicTacToe {
     private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
+    private char lastPlayer = '\0';
 
     public void play(int x, int y) {
         checkAxis(x);
         checkAxis(y);
         setBox(x, y);
+        lastPlayer = nextPlayer();
     }
 
     private void checkAxis(int axis) {
@@ -22,6 +24,9 @@ public class TicTacToe {
     }
 
     public char nextPlayer() {
+        if (lastPlayer == 'X') {
+            return 'O';
+        }
         return 'X';
     }
 }
