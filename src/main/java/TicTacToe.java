ccfbbd3190bb4b4c482bfec18719efb3dd195c23
Unknown <miron.kuzniar@gmail.com>
2017-10-12 14:07:02 +0200

    Adding code for win conditions
    Column full

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -17,12 +17,17 @@ public class TicTacToe {
     }
 
     private boolean isWin() {
+        int playerTotal = lastPlayer * 3;
         for (int i = 0; i < SIZE; i++) {
-            if (board[0][i] + board[1][i] + board[2][i] == (lastPlayer * SIZE)) {
+            if (board[0][i] + board[1][i] + board[2][i] == playerTotal) {
+                return true;
+            } else if (board[i][0] + board[i][1] + board[i][2] == playerTotal) {
                 return true;
             }
-        } return false;
+        }
+        return false;
     }
+
 
     private void checkAxis(int axis) {
         if (axis < 1 || axis > 3) {
