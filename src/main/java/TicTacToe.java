e398eaa16bbb90a32d4d3d950ad704898de49396
Unknown <miron.kuzniar@gmail.com>
2017-10-12 14:19:08 +0200

    Adding code for win conditions
    Diagonal full

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -25,6 +25,9 @@ public class TicTacToe {
                 return true;
             }
         }
+        if ((board[0][0] + board[1][1] + board[2][2]) == playerTotal) {
+            return true;
+        }
         return false;
     }
 
